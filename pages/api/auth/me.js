02284a9b4f37adaca4eabcf5d20bad7d710008c2
jintianhaiyiyang <jintianhaiyiyang@gmail.com>
2026-01-16 18:54:22 +0800

    Enhance meRoute to include isAdmin in response
    Add isAdmin property to user session response.

--- pages/api/auth/me.js
+++ pages/api/auth/me.js
@@ -1,15 +1,15 @@
 import { withIronSessionApiRoute } from "../../../lib/session";
 
 async function meRoute(req, res) {
-  // 直接从 Session 中读取用户信息
   if (req.session.user) {
     res.json({
-      ...req.session.user,
+      ...req.session.user, // 这会包含 isAdmin: true/false
       isLoggedIn: true,
     });
   } else {
     res.json({
       isLoggedIn: false,
+      isAdmin: false,
     });
   }
 }
